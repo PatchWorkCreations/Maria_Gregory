0e87b4ba384d5981a2df3bcace965eaae3e66ec5
Jvictorio09 <juliavictorio16@gmail.com>
2025-11-28 21:21:57 +0800

    udpated

--- myProject/settings.py
+++ myProject/settings.py
@@ -18,9 +18,9 @@ SECRET_KEY = 'django-insecure-&9huwp*tj-e^ez803(em+)&^hl@df25)#bhd0fj&+t+yy)&mil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mariagregory-production.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mariagregory-production.up.railway.app', 'mariagregory.com', 'www.mariagregory.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://localhost', 'https://mariagregory-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://localhost', 'https://mariagregory-production.up.railway.app', 'https://mariagregory.com', 'https://www.mariagregory.com']
 
 # Application definition
 
